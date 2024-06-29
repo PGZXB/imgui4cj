b2933940f5ec0d4d189098a712cc135b657502cb
PGZXB <pgzxb@qq.com>
2024-06-30 03:52:34 +0800

    Impl code gen for enums

--- generate_capi.sh
+++ generate_capi.sh
@@ -2,6 +2,13 @@
 
 path=$(cd $(dirname $0); pwd)
 
+run_args=""
+run_args="$run_args --definitions-json='$path/external/cimgui/generator/output/definitions.json'"
+run_args="$run_args --impl-definitions-json='$path/external/cimgui/generator/output/impl_definitions.json'"
+run_args="$run_args --structs-and-enums-json='$path/external/cimgui/generator/output/structs_and_enums.json'"
+run_args="$run_args --typedefs-dict-json='$path/external/cimgui/generator/output/typedefs_dict.json'"
+run_args="$run_args --capi-pkg-path='$path/src/capi'"
+
 cd "$path/generator" && \
     cjpm build && \
-    cjpm run --run-args --capi-pkg-path,"$path/src/capi"
+    cjpm run --run-args "$run_args"
