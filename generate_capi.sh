1e8e4006ee955e8f701da54954156f73e0a279be
PGZXB <pgzxb@qq.com>
2024-07-08 00:08:14 +0800

    Fix invalid package name (_capi -> capi_impl

--- generate_capi.sh
+++ generate_capi.sh
@@ -7,8 +7,8 @@ run_args="$run_args --definitions-json='$path/external/cimgui/generator/output/d
 run_args="$run_args --impl-definitions-json='$path/external/cimgui/generator/output/impl_definitions.json'"
 run_args="$run_args --structs-and-enums-json='$path/external/cimgui/generator/output/structs_and_enums.json'"
 run_args="$run_args --typedefs-dict-json='$path/external/cimgui/generator/output/typedefs_dict.json'"
-run_args="$run_args --capi-impl-pkg-path='$path/src/_capi'"
-run_args="$run_args --capi-impl-pkg-name='_capi'"
+run_args="$run_args --capi-impl-pkg-path='$path/src/capi_impl'"
+run_args="$run_args --capi-impl-pkg-name='capi_impl'"
 run_args="$run_args --capi-pkg-path='$path/src/capi'"
 run_args="$run_args --capi-pkg-name='capi'"
 
