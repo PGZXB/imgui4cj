fce2b79e5a8d8e2a0fd83d4f4744c2676e9888be
PGZXB <pgzxb@qq.com>
2024-07-09 00:43:28 +0800

    Fix link error during example running

--- build_imgui4cj_c_lib.sh
+++ build_imgui4cj_c_lib.sh
@@ -4,4 +4,4 @@ git submodule update --init --recursive && \
     cmake . -B build && \
     cmake --build build && \
     cp build/imgui4cj.so src/libs/ && \
-    mv src/libs/imgui4cj.so src/libs/libimgui4cj.so
+    cp src/libs/imgui4cj.so src/libs/libimgui4cj.so
