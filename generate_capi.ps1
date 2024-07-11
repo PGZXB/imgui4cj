1da91a98365f6580a388cbb0f8096cf47d3d4017
PGZXB <pgzxb@qq.com>
2024-07-12 00:38:04 +0800

    Make Windows happy

---
+++ generate_capi.ps1
@@ -0,0 +1,17 @@
+$path = (Get-Location).Path
+
+$run_args = ""
+$run_args += " --definitions-json=$path/external/cimgui/generator/output/definitions.json"
+$run_args += " --impl-definitions-json=$path/external/cimgui/generator/output/impl_definitions.json"
+$run_args += " --structs-and-enums-json=$path/external/cimgui/generator/output/structs_and_enums.json"
+$run_args += " --typedefs-dict-json=$path/external/cimgui/generator/output/typedefs_dict.json"
+$run_args += " --capi-impl-pkg-path=$path/src/capi_impl"
+$run_args += " --capi-impl-pkg-name=capi_impl"
+$run_args += " --capi-pkg-path=$path/src/capi"
+$run_args += " --capi-pkg-name=capi"
+$run_args += " --c-src-path=$path/c_src"
+
+Push-Location -Path "$path/generator"
+cjpm build
+cjpm run --run-args $run_args
+Pop-Location
