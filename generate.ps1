d5d6d5e0c45482b129e4b3d78c867b62f6703b32
PGZXB <pgzxb@qq.com>
2024-07-27 03:23:24 +0800

    Update code to be compatible with compiler version 0.53.4 (incompatible with 0.51.4)

--- generate.ps1
+++ generate.ps1
@@ -6,12 +6,12 @@ $run_args += " --impl-definitions-json=$path/external/cimgui/generator/output/im
 $run_args += " --structs-and-enums-json=$path/external/cimgui/generator/output/structs_and_enums.json"
 $run_args += " --typedefs-dict-json=$path/external/cimgui/generator/output/typedefs_dict.json"
 $run_args += " --capi-impl-pkg-path=$path/src/capi_impl"
-$run_args += " --capi-impl-pkg-name=capi_impl"
+$run_args += " --capi-impl-pkg-name=imgui4cj.capi_impl"
 $run_args += " --capi-pkg-path=$path/src/capi"
-$run_args += " --capi-pkg-name=capi"
+$run_args += " --capi-pkg-name=imgui4cj.capi"
 $run_args += " --c-src-path=$path/c_src"
 $run_args += " --wcapi-pkg-path=$path/src/wcapi"
-$run_args += " --wcapi-pkg-name=wcapi"
+$run_args += " --wcapi-pkg-name=imgui4cj.wcapi"
 
 
 Push-Location -Path "$path/generator"
--- generate.sh
+++ generate.sh
@@ -8,12 +8,12 @@ run_args="$run_args --impl-definitions-json='$path/external/cimgui/generator/out
 run_args="$run_args --structs-and-enums-json='$path/external/cimgui/generator/output/structs_and_enums.json'"
 run_args="$run_args --typedefs-dict-json='$path/external/cimgui/generator/output/typedefs_dict.json'"
 run_args="$run_args --capi-impl-pkg-path='$path/src/capi_impl'"
-run_args="$run_args --capi-impl-pkg-name='capi_impl'"
+run_args="$run_args --capi-impl-pkg-name='imgui4cj.capi_impl'"
 run_args="$run_args --capi-pkg-path='$path/src/capi'"
-run_args="$run_args --capi-pkg-name='capi'"
+run_args="$run_args --capi-pkg-name='imgui4cj.capi'"
 run_args="$run_args --c-src-path='$path/c_src'"
 run_args="$run_args --wcapi-pkg-path='$path/src/wcapi'"
-run_args="$run_args --wcapi-pkg-name='wcapi'"
+run_args="$run_args --wcapi-pkg-name='imgui4cj.wcapi'"
 
 cd "$path/generator" && \
     cjpm build && \
