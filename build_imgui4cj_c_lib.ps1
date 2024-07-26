30832307a242f0b6a1dedf72135c03da38e24169
PGZXB <pgzxb@qq.com>
2024-07-26 12:14:51 +0800

    Static link glfw

--- build_imgui4cj_c_lib.ps1
+++ build_imgui4cj_c_lib.ps1
@@ -1,6 +1,5 @@
 git submodule update --init --recursive
 cmake . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
-Copy-Item -Path build\_deps\glfw-build\src\Release\glfw3.dll -Destination src\libs\
 Copy-Item -Path build\Release\imgui4cj.dll -Destination src\libs\
 Copy-Item -Path src\libs\imgui4cj.dll -Destination src\libs\libimgui4cj.dll
