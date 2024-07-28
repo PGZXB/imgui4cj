ef00672e960e26a904f6d1b2984fb49b1cfd8a87
PGZXB <pgzxb@qq.com>
2024-07-28 19:47:37 +0800

    Fix build_imgui4cj_c_lib.{ps1,sh} to match DLL name change

--- build_imgui4cj_c_lib.ps1
+++ build_imgui4cj_c_lib.ps1
@@ -1,5 +1,10 @@
+$buildPath = "build"
+$libsPath = "src\libs"
+$dllName = "imgui4cj_c_lib.dll"
+
 git submodule update --init --recursive
-cmake . -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config Release
-Copy-Item -Path build\Release\imgui4cj.dll -Destination src\libs\
-Copy-Item -Path src\libs\imgui4cj.dll -Destination src\libs\libimgui4cj.dll
+cmake . -B $buildPath -DCMAKE_BUILD_TYPE=Release
+cmake --build $buildPath --config Release
+Remove-Item -Path "$libsPath\*.dll" -Force
+Copy-Item -Path "$buildPath\Release\$dllName" -Destination "$libsPath\$dllName"
+Copy-Item -Path "$buildPath\Release\$dllName" -Destination "$libsPath\lib$dllName"
--- build_imgui4cj_c_lib.sh
+++ build_imgui4cj_c_lib.sh
@@ -1,7 +1,12 @@
-#! /bin/bash
+#!/bin/bash
+
+buildPath="build"
+libsPath="src/libs"
+dllName="imgui4cj_c_lib.so"
 
 git submodule update --init --recursive && \
     cmake . -B build && \
     cmake --build build && \
-    cp build/imgui4cj.so src/libs/ && \
-    cp src/libs/imgui4cj.so src/libs/libimgui4cj.so
+    rm ${libsPath}/*.so && \
+    cp ${buildPath}/${dllName} ${libsPath}/${dllName} && \
+    cp ${buildPath}/${dllName} ${libsPath}/lib${dllName}
