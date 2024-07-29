029e08506bc70a1bebec5ee925bb6425fb0f3a62
PGZXB <pgzxb@qq.com>
2024-07-29 09:17:56 +0800

    Fix build_imgui4cj_c_lib.sh to prevent errors when .so files are not found

--- build_imgui4cj_c_lib.sh
+++ build_imgui4cj_c_lib.sh
@@ -7,6 +7,6 @@ dllName="imgui4cj_c_lib.so"
 git submodule update --init --recursive && \
     cmake . -B build && \
     cmake --build build && \
-    rm ${libsPath}/*.so && \
+    rm -f ${libsPath}/*.so && \
     cp ${buildPath}/${dllName} ${libsPath}/${dllName} && \
     cp ${buildPath}/${dllName} ${libsPath}/lib${dllName}
